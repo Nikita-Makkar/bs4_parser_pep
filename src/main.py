bd8e28b0bf30cdc138ebd07dbb6a47eeb118fd09
Никита Маккар <kish.zk@mail.ru>
2024-05-23 20:42:51 +0300

    add fix

--- src/main.py
+++ src/main.py
@@ -3,21 +3,17 @@ import re
 from urllib.parse import urljoin
 
 import requests_cache
-from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 from configs import configure_argument_parser, configure_logging
 from constants import BASE_DIR, EXPECTED_STATUS, MAIN_DOC_URL, PEP_DOC_URL
 from outputs import control_output
-from utils import find_tag, get_response
+from utils import find_tag, get_soup
 
 
 def whats_new(session):
     whats_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
-    response = get_response(session, whats_new_url)
-    if response is None:
-        return
-    soup = BeautifulSoup(response.text, features='lxml')
+    soup = get_soup(session, whats_new_url)
     main_div = find_tag(soup, 'section', attrs={'id': 'what-s-new-in-python'})
     div_with_ul = find_tag(main_div, 'div', attrs={'class': 'toctree-wrapper'})
     sections_by_python = div_with_ul.find_all(
@@ -29,10 +25,7 @@ def whats_new(session):
         version_a_tag = section.find('a')
         href = version_a_tag['href']
         version_link = urljoin(whats_new_url, href)
-        response = get_response(session, version_link)
-        if response is None:
-            continue
-        soup = BeautifulSoup(response.text, features='lxml')
+        soup = get_soup(whats_new_url, href)
         h1 = find_tag(soup, 'h1')
         dl = find_tag(soup, 'dl')
         dl_text = dl.text.replace('\n', ' ')
@@ -44,10 +37,7 @@ def whats_new(session):
 
 
 def latest_versions(session):
-    response = get_response(session, MAIN_DOC_URL)
-    if response is None:
-        return
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = get_soup(session, MAIN_DOC_URL)
     sidebar = soup.find('div', {'class': 'sphinxsidebarwrapper'})
     ul_tags = sidebar.find_all('ul')
     for ul in ul_tags:
@@ -75,11 +65,7 @@ def latest_versions(session):
 
 def download(session):
     downloads_url = urljoin(MAIN_DOC_URL, 'download.html')
-    response = get_response(session, downloads_url)
-    if response is None:
-        return
-    soup = BeautifulSoup(response.text, 'lxml')
-
+    soup = get_soup(session, downloads_url)
     main_tag = soup.find('div', {'role': 'main'})
     table_tag = main_tag.find('table', {'class': 'docutils'})
     pdf_a4_tag = table_tag.find('a', {'href': re.compile(r'.+pdf-a4\.zip$')})
@@ -97,14 +83,14 @@ def download(session):
 
 
 def pep(session):
-    response = get_response(session, PEP_DOC_URL)
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = get_soup(session, PEP_DOC_URL)
     section_tag = find_tag(soup, 'section', {'id': 'numerical-index'})
     main_table = find_tag(section_tag, 'tbody')
     rows = main_table.find_all('tr')
 
     pep_count = 0
     status_count = {}
+    logs = []
 
     results = [('Статус', 'Количество')]
 
@@ -115,35 +101,35 @@ def pep(session):
             status_long = EXPECTED_STATUS[status_short]
         else:
             status_long = []
-            logging.info(
-                f'В списке есть неверно указанный статус: {status_short}'
-                f'В строке: {pep}'
-            )
+            logs.append(f'В списке есть неверно указанный статус: '
+                        f'{status_short} в строке: {pep}')
+
         pep_link_short = pep.find('a')['href']
         pep_link_full = urljoin(PEP_DOC_URL, pep_link_short)
-        response = get_response(session, pep_link_full)
-        soup = BeautifulSoup(response.text, 'lxml')
+        soup = get_soup(session, pep_link_full)
         dl_table = find_tag(soup, 'dl', {'class': 'rfc2822 field-list simple'})
         status_line = dl_table.find(string='Status')
         if status_line:
             status_parent = status_line.find_parent()
             status_page = status_parent.next_sibling.next_sibling.string
             if status_page not in status_long:
-                logging.info(
+                logs.append(
                     f'Несовпадающие статусы: {pep_link_full}'
-                    f'Статус в карточке: - {status_page}'
-                    f'Ожидаемые статусы: - {status_long}'
-                )
+                    f' Статус в карточке: - {status_page}'
+                    f' Ожидаемые статусы: - {status_long}')
             if status_page in status_count:
                 status_count[status_page] += 1
             else:
                 status_count[status_page] = 1
         else:
-            logging.error(
-                f'На странице PEP {pep_link_full}'
-                'В таблице нет строки статуса.'
-            )
+            logs.append(
+                f'На странице PEP {pep_link_full} '
+                f'в таблице нет строки статуса.')
             continue
+
+        for log in logs:
+            logging.info(log)
+
     results.extend(status_count.items())
     results.append(('Total', pep_count))
     return results
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import logging
 
+from bs4 import BeautifulSoup
 from requests import RequestException
+
 from exceptions import ParserFindTagException
 
 
@@ -23,3 +25,11 @@ def find_tag(soup, tag, attrs=None):
         logging.error(error_msg, stack_info=True)
         raise ParserFindTagException(error_msg)
     return searched_tag
+
+
+def get_soup(session, url):
+    response = get_response(session, url)
+    if response is None:
+        return
+    soup = BeautifulSoup(response.text, features='lxml')
+    return soup
